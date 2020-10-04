d5bbfbf0df6adebda9625fb3c04a18fc3085b81f
Justine Estanislao <jpatz.e99@gmail.com>
2020-10-03 22:23:13 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,35 +4,44 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-const budget = [
-    {
-        title: 'Eat out',
-        budget: 50
-    },
-    {
-        title: 'Rent',
-        budget: 390
-    },
-    {
-        title: 'Groceries',
-        budget: 100
-    },
-    {
-        title: 'Car Insurance',
-        budget: 70
-    },
-    {
-        title: 'Entertainment',
-        budget: 130
-    }
-];
+const budgett = require("./server2");
+// const budget = [
+//     {
+//         title: 'Eat out',
+//         budget: 50
+//     },
+//     {
+//         title: 'Rent',
+//         budget: 390
+//     },
+//     {
+//         title: 'Groceries',
+//         budget: 100
+//     },
+//     {
+//         title: 'Car Insurance',
+//         budget: 70
+//     },
+//     {
+//         title: 'Entertainment',
+//         budget: 130
+//     },
+//     {
+//         title: 'Shopping',
+//         budget: 150
+//     },
+//     {
+//         title: 'Subscriptions',
+//         budget: 15
+//     }
+// ];
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(budgett);
 });
 
 app.listen(port, () => {
