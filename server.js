70317ae098f763a178d99eff9007997c13055c78
Justine Estanislao <jpatz.e99@gmail.com>
2020-10-08 16:17:45 -0400

    CORS update

--- server.js
+++ server.js
@@ -1,49 +1,17 @@
+//Budget API
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(cors());
 
 const budgett = require("./server2");
-// const budget = [
-//     {
-//         title: 'Eat out',
-//         budget: 50
-//     },
-//     {
-//         title: 'Rent',
-//         budget: 390
-//     },
-//     {
-//         title: 'Groceries',
-//         budget: 100
-//     },
-//     {
-//         title: 'Car Insurance',
-//         budget: 70
-//     },
-//     {
-//         title: 'Entertainment',
-//         budget: 130
-//     },
-//     {
-//         title: 'Shopping',
-//         budget: 150
-//     },
-//     {
-//         title: 'Subscriptions',
-//         budget: 15
-//     }
-// ];
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
 
 app.get('/budget', (req, res) => {
     res.json(budgett);
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}')
+    console.log('API served at http://localhost:${port}')
 });
